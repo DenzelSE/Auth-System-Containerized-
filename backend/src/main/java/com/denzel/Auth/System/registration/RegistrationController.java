d25eecc85f370c12f5b155cd1162a68240a7cd2e
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-06-09 18:37:46 +0200

    working on registration Repo

--- backend/src/main/java/com/denzel/Auth/System/registration/RegistrationController.java
+++ backend/src/main/java/com/denzel/Auth/System/registration/RegistrationController.java
@@ -1,12 +1,19 @@
 package com.denzel.Auth.System.registration;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.denzel.Auth.System.appuser.Repository.RegistrationService;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/registration")
@@ -16,9 +23,14 @@ import lombok.ToString;
 
 public class RegistrationController {
     
-    private RegistrationService registrationService;
+    private RegistrationService service;
 
-    public String register(@RequestBody RegistrationRequest request){
-        return registrationService.register(request);
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(
+        @RequestBody @Valid RegistrationRequest request
+    ){
+        service.register(request);
+        return ResponseEntity.accepted().build();
     }
 }
--- backend/src/main/java/com/denzel/Auth/System/registration/RegistrationRequest.java
+++ backend/src/main/java/com/denzel/Auth/System/registration/RegistrationRequest.java
@@ -1,17 +1,31 @@
 package com.denzel.Auth.System.registration;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
-
+@Setter
+@Builder
 public class RegistrationRequest {
    
-    private final String firstName;
-    private final String lastName;
-    private final String email;
-    private final String password;
-
+    @NotEmpty(message = "Firstname is mandatory")
+    @NotBlank(message = "Firstname is mandatory")
+    private String firstname;
+    @NotEmpty(message = "Firstname is mandatory")
+    @NotBlank(message = "Firstname is mandatory")
+    private String lastname;
+    @NotEmpty(message = "Firstname is mandatory")
+    @NotBlank(message = "Firstname is mandatory")
+    private String email;
+    @Email(message="Email is not formatted")
+    @NotEmpty(message = "Firstname is mandatory")
+    @NotBlank(message = "Firstname is mandatory")
+    @Size(min = 6, message = "Passwaord should be 6 characters long minimum")
+    private String password;
 
 }
