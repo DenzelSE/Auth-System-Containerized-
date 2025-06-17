e0d339178e0c834ceaf7764da4f65d8e924230f0
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-06-17 18:57:55 +0200

    proper handling

--- backend/src/main/java/com/denzel/Auth/System/auth/RegistrationRequest.java
+++ backend/src/main/java/com/denzel/Auth/System/auth/RegistrationRequest.java
@@ -17,15 +17,15 @@ public class RegistrationRequest {
     @NotEmpty(message = "Firstname is mandatory")
     @NotBlank(message = "Firstname is mandatory")
     private String firstname;
-    @NotEmpty(message = "Firstname is mandatory")
-    @NotBlank(message = "Firstname is mandatory")
+    @NotEmpty(message = "lasttname is mandatory")
+    @NotBlank(message = "lasttname is mandatory")
     private String lastname;
     @Email(message = "Email is not well formmated")
-    @NotEmpty(message = "Firstname is mandatory")
-    @NotBlank(message = "Firstname is mandatory")
+    @NotEmpty(message = "emailname is mandatory")
+    @NotBlank(message = "emailname is mandatory")
     private String email;
-    @NotEmpty(message = "Firstname is mandatory")
-    @NotBlank(message = "Firstname is mandatory")
+    @NotEmpty(message = "password is mandatory")
+    @NotBlank(message = "password is mandatory")
     @Size(min = 6, message = "Passwaord should be 6 characters long minimum")
     private String password;
 
